f06bb067a38ac6462ee01db42ae8c83ff55cca38
张瀚宸 <bjhansen2012@outlook.com>
2021-06-13 11:36:33 +0800

    feat: connect to java part

--- run.py
+++ run.py
@@ -11,7 +11,11 @@ import requests
 import stomper
 import websocket
 
-API_URL = environ.get('API_URL')
+from subprocess import Popen, PIPE
+
+API_URL = environ.get('API_URL') or "https://chess.cslab.top/api"
+JAR_PATH = environ.get('JAR_PATH') or "./flamechess.jar"
+
 if API_URL:
     while API_URL[-1] == '/':
         API_URL = API_URL[:-1]
@@ -24,6 +28,14 @@ def generate_sub_id():
     timestamp = floor(time() * 1000)
     random_id = randint(0, 999)
     return f'sub-{timestamp}-{random_id:03}'
+
+
+def z2state(z: str) -> List[List[int]]:
+    return [[{"z": -1, "Z": 1, "0": 0}[j] for j in z[i:i + 14]] for i in range(0, 196, 14)]
+
+
+def state2z(state: List[List[int]]) -> str:
+    return "".join(["".join([{-1: "z", 0: "0", 1: "Z"}[j] for j in i]) for i in state])
 
 
 class Client(websocket.WebSocketApp):
@@ -54,6 +66,11 @@ class Client(websocket.WebSocketApp):
     @staticmethod
     def on_open(ws: 'Client'):
         ws.send('CONNECT\naccept-version:1.0,1.1,2.0\n\n\x00\n')
+        ws.p = Popen(f"java -cp {JAR_PATH} com.jingbh.flamechess.jiuqi.MCTSRunnerWithUser -t 4",
+                     shell=True, stdin=PIPE, stdout=PIPE, encoding="utf-8")
+        ws.p.stdin.write("-1\n")
+        ws.p.stdin.flush()
+        ws.p.stdout.readline()
 
     @staticmethod
     def on_message(ws: 'Client', message):
@@ -64,7 +81,18 @@ class Client(websocket.WebSocketApp):
             state: List[List[int]] = json.loads(decoded_message['body'])
             if not ws.last_state or json.dumps(state) != json.dumps(ws.last_state):
                 ws.last_state = state
-                print(state)  # TODO: Process it.
+                z = state2z(state)
+                print(z)
+                ws.p.stdin.write(z+"\n")  # write it
+                ws.p.stdin.flush()
+                ws.p.stdin.write("0\n")     # stage
+                ws.p.stdin.flush()
+                respond = ws.p.stdout.readline().strip()
+                ws.p.stdout.readline()    # read "Input State:"
+                print("respond:", respond)
+                ws.last_state = z2state(respond)
+                ws.update_board(z2state(respond))
+                print("updated")
                 # Then use `ws.update_board(new_state)` to update the board.
 
     @staticmethod
